d52ed3536bac74a51333fdef79e185ff88e158cc
Jeff Everhart <Jeffeverhart383@gmail.com>
2016-11-21 14:42:16 -0500

    add code to display details

--- popup.js
+++ popup.js
@@ -1,9 +1,82 @@
 document.addEventListener("DOMContentLoaded", function(){
-	
-
-	var popupLinksObject; 
 
 	
+	var popupLinksObject;
+
+	
+	/*******
+	Functions to display link details 
+	********/
+
+	//DOM vars to cache for details functions
+		var linkDetailsDisplay = document.querySelector('#link-details-display');
+		var linksDiv = document.querySelector('#links');
+		var linksDetailsDiv = document.querySelector('#link-details');
+
+	function showLinkDetails(typeOfLinks){
+
+		//Clear links div of previously appended children 
+		var elemsToRemove = document.querySelectorAll('#link-details-display p'); 
+		for (var i = 0; i < elemsToRemove.length; i++){
+			linkDetailsDisplay.removeChild(elemsToRemove[i]);
+		}
+
+		linksDiv.style.display = "none";
+		linksDetailsDiv.style.display = "block";
+
+		var linksArray;
+		switch (typeOfLinks){
+			case "relativeLinks":
+				linksArray = popupLinksObject.relativeLinks;
+				break;
+			case "absoluteLinks":
+				linksArray = popupLinksObject.absoluteLinks;
+				break;
+			case "hashLinks": 
+				linksArray = popupLinksObject.hashLinks;
+				break;
+		}
+
+		console.log(linksArray);
+		for (var i = 0; i < linksArray.length; i++){
+			var newElem = document.createElement('p');
+			var linkString = linksArray[i].href;
+			newElem.innerHTML = linkString;
+			linkDetailsDisplay.appendChild(newElem);
+		}
+
+	}
+
+	function hideLinkDetails(){
+		linksDiv.style.display = "block";
+		linksDetailsDiv.style.display = "none";
+	}
+
+
+	var relDetailsBtn = document.getElementById('relDetailsBtn');
+
+	relDetailsBtn.addEventListener('click', function(){
+		showLinkDetails("relativeLinks");
+	});
+
+	var absDetailsBtn = document.getElementById('absDetailsBtn');
+
+	absDetailsBtn.addEventListener('click', function(){
+		showLinkDetails("absoluteLinks");
+	});
+
+	var hashDetailsBtn = document.getElementById('hashDetailsBtn');
+
+	hashDetailsBtn.addEventListener('click', function(){
+		showLinkDetails("hashLinks");
+	});
+
+	var backToLinksBtn = document.getElementById('backToLinks');
+
+	backToLinksBtn.addEventListener('click', function(){
+		hideLinkDetails();
+	});	
+
 
 
 	/******
